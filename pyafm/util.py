30ec37f15c9eb1a76fba4ab8d2ebafa1fb048356
Chris MacLellan <2348-cm3786@users.noreply.gitlab.cci.drexel.edu>
2020-09-29 13:56:43 -0400

    created helper for predicting with afm

--- pyafm/util.py
+++ pyafm/util.py
@@ -7,40 +7,6 @@ from math import exp
 
 import numpy as np
 
-def afm_predict(student_step_rollup, kc_model_name):
-    ssr_file = student_step_rollup
-
-    kcs, opps, y, stu, student_label, item_label = read_datashop_student_step(
-        ssr_file, kc_model_name)
-
-    # Get everything in the right matrix format
-    sv = DictVectorizer()
-    qv = DictVectorizer()
-    ov = DictVectorizer()
-    S = sv.fit_transform(stu)
-    Q = qv.fit_transform(kcs)
-    O = ov.fit_transform(opps)
-    X = hstack((S, Q, O))
-    y = np.array(y)
-
-    # Regularize the student intercepts
-    l2 = [1.0 for i in range(S.shape[1])]
-    l2 += [0.0 for i in range(Q.shape[1])]
-    l2 += [0.0 for i in range(O.shape[1])]
-
-    # Bound the learning rates to be positive
-    bounds = [(None, None) for i in range(S.shape[1])]
-    bounds += [(None, None) for i in range(Q.shape[1])]
-    bounds += [(0, None) for i in range(O.shape[1])]
-
-    X = X.toarray()
-    X2 = Q.toarray()
-
-    afm = CustomLogistic(bounds=bounds, l2=l2, fit_intercept=False)
-    afm.fit(X, y)
-    yAFM = afm.predict_proba(X)
-
-    return yAFM
 
 def log_one_plus_exp(z):
     """
@@ -52,6 +18,7 @@ def log_one_plus_exp(z):
     else:
         return log(1 + exp(z))
 
+
 def invlogit(z):
     """
     This function return 1 / (1 + exp(-z)) where it rewrites the terms to
@@ -62,7 +29,6 @@ def invlogit(z):
     else:
         return exp(z) / (1 + exp(z))
 
+
 invlogit_vect = np.vectorize(invlogit)
 log_one_plus_exp_vect = np.vectorize(log_one_plus_exp)
-
-
